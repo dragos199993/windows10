ea9b3f56bf27cd97962bde2a65bdd7a19728962d
Nedsilon <dragos199993@gmail.com>
2018-05-29 23:16:48 +0300

    first

---
+++ precache-manifest.510ab2a846afde07dbca100494ba4785.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "597f49bc7bc430df0f3e",
+    "url": "/js/chunk-vendors.597f49bc.js"
+  },
+  {
+    "revision": "4e2573c6830f5a73ea7c",
+    "url": "/js/app.4e2573c6.js"
+  },
+  {
+    "revision": "1362d436febd4671dc0ce600f942e470",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d40b75e453033c39529b5dc39d7a857",
+    "url": "/img/windows10.9d40b75e.jpg"
+  },
+  {
+    "revision": "4e2573c6830f5a73ea7c",
+    "url": "/css/app.b5efcea8.css"
+  }
+];
